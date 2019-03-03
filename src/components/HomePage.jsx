5974f2378e13b140bc0eaaf2c12160fbb33b1c5e
threedliams <threedliams@gmail.com>
2019-03-02 19:56:46 -0800

    added title to homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,6 +5,7 @@ const createReactClass = require('create-react-class');
 
 export default createReactClass({
     render: function() {
+        document.title = "Ramsey Opp - Software Developer";
         return (
             <div className="home-page">
                 <TopBar />
