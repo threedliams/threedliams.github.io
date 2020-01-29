991a0fd18ea6b44a44109606810af9e95036664c
threedliams <threedliams@gmail.com>
2020-01-28 21:42:57 -0800

    simplify project list return by pulling variables out and mapping

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import TopBar from './TopBar';
-import InternalLink from './InternalLink';
 import ProjectListItem from './ProjectListItem';
 
 export default class Projects extends React.Component {
@@ -8,61 +7,70 @@ export default class Projects extends React.Component {
 		document.title = "Projects";
 	}
 	render() {
+		const posts = [
+			{
+				name: "CallbackBot (narcov)",
+				about: "/projects/narcov",
+				repo: "https://github.com/threedliams/CallbackBot",
+				image: "https://ramseyopp.com/pages/assets/projectThumbnails/narcov.png",
+			},
+			{
+				name: "Particle Filter Localization",
+				about: "/projects/localization",
+				repo: "https://github.com/EricFalkenberg/particle_filter_localization",
+				image: "https://ramseyopp.com/pages/assets/projectThumbnails/particleFilter.png",
+			},
+			{
+				name: "HughBorg",
+				about: "/projects/hughborg",
+				repo: "https://github.com/threedliams/HughBorg",
+				image: "https://ramseyopp.com/pages/assets/projectThumbnails/hughborg.png",
+			},
+			{
+				name: "This website",
+				about: "/projects/website",
+				repo: "https://github.com/threedliams/threedliams.github.io",
+				image: "https://ramseyopp.com/pages/assets/projectThumbnails/thisWebsite.png",
+			},
+			{
+				name: "Crappy Games",
+				about: "/projects/crappyGames",
+				repo: "https://github.com/threedliams/CrappyGames",
+				play: "https://threedliams.github.io/pages/assets/games/CrappyGames/bin/index.html",
+				image: "https://ramseyopp.com/pages/assets/projectThumbnails/crappyGames.PNG",
+			},
+			{
+				name: "MonkeyProto",
+				about: "/projects/monkeyProto",
+				repo: "https://github.com/threedliams/MonkeyProto",
+				play: "https://threedliams.github.io/pages/assets/games/MonkeyProto/bin/index.html",
+				playComment: " (only works with 2 controllers)",
+				image: "https://ramseyopp.com/pages/assets/projectThumbnails/monkeyProto.PNG",
+			},
+			{
+				name: "Old Roguelike",
+				about: "/projects/oldRoguelike",
+				repo: "https://github.com/threedliams/MutationRoguelike",
+				image: "https://ramseyopp.com/pages/assets/projectThumbnails/oldRoguelike.png",
+			},
+		]
+
 		return (
 			<div>
 				<TopBar />
 				<ul className="project-list">
-					<ProjectListItem
-						name="CallbackBot (narcov)"
-						about="/projects/narcov"
-						repo="https://github.com/threedliams/CallbackBot"
-						image="https://ramseyopp.com/pages/assets/projectThumbnails/narcov.png"
-					/>
-
-					<ProjectListItem
-						name="Particle Filter Localization"
-						about="/projects/localization"
-						repo="https://github.com/EricFalkenberg/particle_filter_localization"
-						image="https://ramseyopp.com/pages/assets/projectThumbnails/particleFilter.png"
-					/>
-
-					<ProjectListItem
-						name="HughBorg"
-						about="/projects/hughborg"
-						repo="https://github.com/threedliams/HughBorg"
-						image="https://ramseyopp.com/pages/assets/projectThumbnails/hughborg.png"
-					/>
-
-					<ProjectListItem
-						name="This website"
-						about="/projects/website"
-						repo="https://github.com/threedliams/threedliams.github.io"
-						image="https://ramseyopp.com/pages/assets/projectThumbnails/thisWebsite.png"
-					/>
-
-					<ProjectListItem
-						name="Crappy Games"
-						about="/projects/crappyGames"
-						repo="https://github.com/threedliams/CrappyGames"
-						play="https://threedliams.github.io/pages/assets/games/CrappyGames/bin/index.html"
-						image="https://ramseyopp.com/pages/assets/projectThumbnails/crappyGames.PNG"
-					/>
-
-					<ProjectListItem
-						name="MonkeyProto"
-						about="/projects/monkeyProto"
-						repo="https://github.com/threedliams/MonkeyProto"
-						play="https://threedliams.github.io/pages/assets/games/MonkeyProto/bin/index.html"
-						playComment=" (only works with 2 controllers)"
-						image="https://ramseyopp.com/pages/assets/projectThumbnails/monkeyProto.PNG"
-					/>
-
-					<ProjectListItem
-						name="Old Roguelike"
-						about="/projects/oldRoguelike"
-						repo="https://github.com/threedliams/MutationRoguelike"
-						image="https://ramseyopp.com/pages/assets/projectThumbnails/oldRoguelike.png"
-					/>
+					{
+						posts.map((item) => {
+							return <ProjectListItem
+								name={item.name}
+								about={item.about}
+								repo={item.repo}
+								play={item.play}
+								playComment={item.playComment}
+								image={item.image}
+							/>
+						})
+					}
 				</ul>
 			</div>
 		);
