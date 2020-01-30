984b3916e5780bf8bb29224dd3b8112eedf9b90d
threedliams <threedliams@gmail.com>
2020-01-29 20:28:03 -0800

    reordered pages in menu, changes devlog to thoughts

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export default class TopBar extends React.Component {
 	render() {
 		return <ul className="home-links">
-			<li><Link to="/post-list">Devlog</Link></li>
-			<li><Link to="/projects">Projects</Link></li>
 			<li><Link to="/about">About</Link></li>
+			<li><Link to="/projects">Projects</Link></li>
+			<li><Link to="/post-list">Thoughts</Link></li>
 			<li><Link to="/resume">Résumé</Link></li>
 		</ul>;
 	}
