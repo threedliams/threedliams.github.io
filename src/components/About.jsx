2ed1cd48f2db8bb4e86a28d4c0271fb5931a076b
threedliams <threedliams@gmail.com>
2020-10-11 16:18:25 -0700

    Dynamically calculate the number of years I've been playing piano

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,17 @@ import ReactMarkdown from 'react-markdown';
 export default class About extends React.Component {
 	componentDidMount() {
 		document.title = "About";
+
+		const startDate = new Date(2002, 3);
+		const now = new Date();
+		document.getElementById("pianoYears").innerHTML = Math.floor((now - startDate) / (1000 * 3600 * 24 * 365));
 	}
 
 	render() {
 		return (
 			<div className="about">
 				<TopBar/>
-				<ReactMarkdown source={aboutText}/>
+				<ReactMarkdown source={aboutText} escapeHtml={false}/>
 			</div>
 		);
 	}
